b6413dbf0e24cd32fc8a595b97ce8fe6bc8b3dfd
Aadarsha Khatri <adarskhatri1@gmail.com>
2025-02-19 18:23:27 +0545

    feat: Authenicating added

--- app/(authenticated)/sessions.ts
+++ app/(authenticated)/sessions.ts
@@ -2,9 +2,7 @@
 import { SignJWT, jwtVerify } from 'jose'
 import { cookies } from 'next/headers'
 
-
-
-const secretKey = process.env.SESSION_SECRET
+const secretKey = process.env.TOKEN_SECRET
 const encodedKey = new TextEncoder().encode(secretKey)
 
 
@@ -21,15 +19,17 @@ export async function encrypt(payload : SessionPayload) {
     .sign(encodedKey)
 }
  
-export async function decrypt(session: string | undefined = '') {
+export async function decrypt(session: string) {
   try {
+
     const { payload } = await jwtVerify(session, encodedKey, {
       algorithms: ['HS256'],
     })
+ 
     return payload
   } catch (error) {
-    console.log('Failed to verify session');
-    console.log(error);
+    console.log('Failed to verify session',error);
+    
   }
 }
 
@@ -45,28 +45,6 @@ export async function createSession(userId: string) {
   })
 
 }
-
-
-export async function updateSession() {
-  const session = (await cookies()).get('session')?.value
-  const payload = await decrypt(session)
- 
-  if (!session || !payload) {
-    return null
-  }
- 
-  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
- 
-  const cookieStore = await cookies()
-  cookieStore.set('session', session, {
-    httpOnly: true,
-    secure: true,
-    expires: expires,
-    sameSite: 'lax',
-    path: '/',
-  })
-}
-
 
 export async function deleteSession() {
   const cookieStore = await cookies()
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,19 @@
-"use client"
+"use server"
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { decrypt } from "./(authenticated)/sessions";
+import { decrypt } from "./app/(authenticated)/sessions";
 
 const protectedRoutes = ["/admin/site-controller"];
-const publicRoutes = ["/admin/login", "/"];
+const publicRoutes = ["/admin/login",];
 
-export default async function middleware(req: NextRequest) {
-  console.log("Middleware Running...");
+export async function middleware(req: NextRequest) {
+
   try {
+    console.log(req.nextUrl);
     const path = req.nextUrl.pathname;
+    console.log(path);
+
+    if(path ==="/") return NextResponse.next();
 
     const isProtected = protectedRoutes.includes(path);
     const isPublic = publicRoutes.includes(path);
@@ -17,12 +21,11 @@ export default async function middleware(req: NextRequest) {
     //Get the Cookies from session
     const cookieStore = await cookies();
     const cookie = cookieStore.get("session")?.value;
-    console.log(`Cookies:${cookie}`);
-
+   
     if(!cookie) console.log("Not Cookie Found!")
       
-    const session = await decrypt(cookie);
-    console.log(session);
+    const session = await decrypt(String(cookie));
+
 
     if (isProtected && !session?.userId) {
       return NextResponse.redirect(new URL("/admin/login", req.nextUrl));
@@ -30,7 +33,7 @@ export default async function middleware(req: NextRequest) {
 
     if (isPublic && session?.userId) {
       return NextResponse.redirect(
-        new URL("admin/site-controller", req.nextUrl)
+        new URL("/admin/site-controller", req.nextUrl)
       );
     }
 
